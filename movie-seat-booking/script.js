2480751994ef454266bcfb2aa9fe8b443f628421
kalevnakah <calebschmidt@gmail.com>
2022-05-05 01:20:11 -0500

    Finished CSS and Started JS

---
+++ movie-seat-booking/script.js
@@ -0,0 +1,31 @@
+const parkinglot = document.querySelector('.parkinglot');
+const parkingspots = document.querySelectorAll('.strip .parkingspot:not(.reserved)');
+const spotcount = document.getElementById('spotcount');
+const sumcost = document.getElementById('sumcost');
+const showSelect = document.getElementById('show');
+
+let showPrice = +showSelect.value;
+
+// Update sumcost and sumcount
+function updatePickedCount() {
+  const pickedSpots = document.querySelectorAll('.strip .parkingspot.picked');
+  const pickedSpotsCount = pickedSpots.length;
+  spotcount.innerText = pickedSpotsCount;
+  sumcost.innerText = pickedSpotsCount * showPrice;
+}
+
+// Show picked event
+show.addEventListener('change', e => {
+  showPrice = +e.target.value;
+  updatePickedCount;
+})
+
+parkinglot.addEventListener('click', e => {
+  if(e.target.classList.contains('parkingspot') &&
+  !e.target.classList.contains('reserved')
+  ) {
+    e.target.classList.toggle('picked');
+  
+    updatePickedCount();
+  }
+})
