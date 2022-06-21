fbd8d18a7fc5375bcd41721a1b0aafbb245280ca
kalevnakah <calebschmidt@gmail.com>
2022-06-20 22:32:21 -0500

    Speech Text Reader: Lesson 1

---
+++ speech-text-reader/script.js
@@ -0,0 +1,76 @@
+const main = document.querySelector('main');
+const voicesSel = document.getElementById('voices');
+const stringBox = document.getElementById('string');
+const readBtn = document.getElementById('read');
+const flipBtn = document.getElementById('flip');
+const shutBtn = document.getElementById('shut');
+
+const data = [
+  {
+    image: './img/drink.jpg',
+    text: "I'm Thirsty",
+  },
+  {
+    image: './img/food.jpg',
+    text: "I'm Hungry",
+  },
+  {
+    image: './img/tired.jpg',
+    text: "I'm Tired",
+  },
+  {
+    image: './img/hurt.jpg',
+    text: "I'm Hurt",
+  },
+  {
+    image: './img/happy.jpg',
+    text: "I'm Happy",
+  },
+  {
+    image: './img/angry.jpg',
+    text: "I'm Angry",
+  },
+  {
+    image: './img/sad.jpg',
+    text: "I'm Sad",
+  },
+  {
+    image: './img/scared.jpg',
+    text: "I'm Scared",
+  },
+  {
+    image: './img/outside.jpg',
+    text: 'I Want To Go Outside',
+  },
+  {
+    image: './img/home.jpg',
+    text: 'I Want To Go Home',
+  },
+  {
+    image: './img/school.jpg',
+    text: 'I Want To Go To School',
+  },
+  {
+    image: './img/grandma.jpg',
+    text: 'I Want To Go To Grandmas',
+  },
+];
+
+data.forEach(createBox);
+
+// Create speech boxes
+function createBox(item) {
+  const box = document.createElement('div');
+
+  const { image, text } = item;
+
+  box.classList.add('box');
+  box.innerHTML = `
+    <img src="${image}" alt="${text}" />
+    <p class="data">${text}</p>
+  `;
+
+  // @Todo - speak event
+
+  main.appendChild(box);
+}
